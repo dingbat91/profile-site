22eee858720f5325be13007a33b58129856c15e6
Matthew Hanson <dingbat91@gmail.com>
2023-01-04 13:49:02 +0000

    Basic Text entry

--- app/Components/Textbox/Textbox.tsx
+++ app/Components/Textbox/Textbox.tsx
@@ -1,5 +1,24 @@
+"use client";
+
+import React, { useState } from "react";
+import { createEditor } from "Slate";
+import { Slate, Editable, withReact } from "slate-react";
+
 function Textbox() {
-	return <div></div>;
+	const [editor] = useState(() => withReact(createEditor()));
+
+	const initialValue = [
+		{
+			type: "Paragraph",
+			children: [{ text: "Editor Text" }],
+		},
+	];
+
+	return (
+		<Slate editor={editor} value={initialValue}>
+			<Editable />
+		</Slate>
+	);
 }
 
 export default Textbox;
