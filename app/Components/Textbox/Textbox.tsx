9823cc46408f60e299634d32d02fccfb4f69e271
Matthew Hanson <dingbat91@gmail.com>
2023-01-05 11:49:15 +0000

    Current work 05/01/2023

--- app/Components/Textbox/Textbox.tsx
+++ app/Components/Textbox/Textbox.tsx
@@ -1,15 +1,64 @@
 "use client";
 
 import React, { useState } from "react";
-import { createEditor } from "Slate";
-import { Slate, Editable, withReact } from "slate-react";
+import {
+	BaseEditor,
+	createEditor,
+	Descendant,
+} from "Slate";
+import { Slate, Editable, withReact, ReactEditor } from "slate-react";
+import { RenderElementProps } from "slate-react/dist/components/editable";
+
+// Typescript Declarations-----------------------------------------------------
+
+type ParagraphElement = {
+	type: "paragraph";
+	align?: string;
+	children: CustomText[];
+};
+
+type CodeElement = {
+	type: "code";
+	align?: string;
+	children: CustomText[];
+};
+
+type CustomElement = ParagraphElement | CodeElement;
+
+type CustomText = {
+	text: string;
+	bold?: true;
+	italic?: true;
+	underline?: true;
+};
+
+declare module "slate" {
+	interface CustomTypes {
+		Editor: BaseEditor & ReactEditor;
+		Element: CustomElement;
+		Text: CustomText;
+	}
+}
+
+//----------------------------------------------------------------
+
+//Elements--------------------------------------------------------
+const CodeElement = (props:RenderElementProps) => {
+return (
+	<pre {...props.attributes}>
+		<code>{props.children}</code>
+	</pre>
+)
+
+}
+//----------------------------------------------------------------
 
 function Textbox() {
 	const [editor] = useState(() => withReact(createEditor()));
 
-	const initialValue = [
+	const initialValue: Descendant[] = [
 		{
-			type: "Paragraph",
+			type: "paragraph",
 			children: [{ text: "Editor Text" }],
 		},
 	];
